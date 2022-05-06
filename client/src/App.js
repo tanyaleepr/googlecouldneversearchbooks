c924e12d80b70a2e244f48a18bf31a3e48f37938
tanyaleepr <tanyaleepr@gmail.com>
2022-05-05 20:59:02 -0500

    verifying if this works pt 100

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,56 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost'
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import Navbar from "./components/Navbar";
 
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-// const client = new ApolloClient({
-//   request: (operation) => {
-//     const token = localStorage.getItem("id_token");
-
-//     operation.setContext({
-//       headers: {
-//         authorization: token ? `Bearer ${token}` : "",
-//       },
-//     });
-//   },
-//   uri: "/graphql",
-// });
+// Link to the GraphQL server on the backend
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
-
+// "Middleware" function to retrieve token and combine it with the existing httpLink
+// We do not need the first parameter offered by setContext. Because of that, "_" is used as a placeholder to get to the second.
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   return {
+    /* ensures the return headers of every request includes the token */
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
-
+// Instantiate the Apollo Client instance to connect to the API endpoint
 const client = new ApolloClient({
+  // Combining authLink with httpLink
   link: authLink.concat(httpLink),
+  // Instantiate a new cache object
   cache: new InMemoryCache(),
 });
 
 function App() {
   return (
-  <ApolloProvider  client ={client}>
-       <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
-  </ApolloProvider>
- 
+    // Enable entire app to interact with the Apollo Client instance
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={SearchBooks} />
+            <Route exact path="/saved" component={SavedBooks} />
+            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
